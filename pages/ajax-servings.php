ca8c697e7905a76c53c22952bd5cd6e1cfb44d3c
Olivier Lüthy <64250640+olivierluethy@users.noreply.github.com>
2024-08-23 14:38:29 +0200

    Check endpoints from ajax-servings.php

--- pages/ajax-servings.php
+++ pages/ajax-servings.php
@@ -3,22 +3,28 @@ include '../configuration.php';
 include '../classes/db-connection.php';
 include '../classes/food.php';
 
-if(isset($_GET['id'])) {
-    
-    $ItemID     = $_GET['id'];
-    $ArrayItems = array();
-    $dbFood     = new dbFood();
-    $dbServings = $dbFood->dbServings($ItemID);
+if (isset($_GET['id'])) {
+    // Überprüfen und Bereinigen des ID-Wertes
+    $ItemID = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8');
 
-    while ($row = mysqli_fetch_assoc($dbServings)) {
-        $ArrayItems[] = $row;
+    // Stellen Sie sicher, dass die ID eine Ganzzahl ist
+    if (filter_var($ItemID, FILTER_VALIDATE_INT)) {
+        $ArrayItems = array();
+        $dbFood = new dbFood();
+        $dbServings = $dbFood->dbServings($ItemID);
+
+        while ($row = mysqli_fetch_assoc($dbServings)) {
+            $ArrayItems[] = $row;
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($ArrayItems);
+    } else {
+        // Wenn die ID keine gültige Ganzzahl ist
+        echo "Invalid ID parameter.";
     }
-    
-    header('Content-Type: application/json');
-    echo json_encode($ArrayItems);
-
 } else {
-    echo "ID parameter is not set in the URL";
+    echo "ID parameter is not set in the URL.";
 }
 
 include 'footer.php';
