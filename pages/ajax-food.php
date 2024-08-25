72cc3debd800db5a9228607976b6ff5a0de7486c
Basil Tschopp <b.tschopp@gmail.com>
2024-08-26 01:10:19 +0200

    Transfer the security issue fix from Ajax-servings to Ajax-food

--- pages/ajax-food.php
+++ pages/ajax-food.php
@@ -5,20 +5,28 @@ include '../classes/food.php';
 
 if(isset($_GET['id'])) {
 
-    $ItemID     = $_GET['id'];
-    $ArrayItems = array();  
-    $dbFood     = new dbFood();
-    $dbItem     = $dbFood->dbItem($ItemID);
+    $ItemID     = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8');
 
-    while ($row = mysqli_fetch_assoc($dbItem)) {
-        $ArrayItems[] = $row;
+    if (filter_var($ItemID, FILTER_VALIDATE_INT)) {
+        $ArrayItems = array();  
+        $dbFood     = new dbFood();
+        $dbItem     = $dbFood->dbItem($ItemID);
+
+        while ($row = mysqli_fetch_assoc($dbItem)) {
+            $ArrayItems[] = $row;
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($ArrayItems);
+
+    } else {
+
+        echo "Invalid ID parameter.";
     }
-
-    header('Content-Type: application/json');
-    echo json_encode($ArrayItems);
     
 } else {
-    echo "ID parameter is not set in the URL";
+
+    echo "ID parameter is not set in the URL.";
 }
 
 include 'footer.php';
